edf867e4fa2370268e5d23d3a1f29ecd81b2be25
KenllyChacon <klchacon@uce.edu.ec>
2023-02-04 10:54:00 -0500

    Taller_26

--- src/main/java/com/uce/edu/ec/programacion_pw_u3_kc_2/controller/EstudianteControllerRestFul.java
+++ src/main/java/com/uce/edu/ec/programacion_pw_u3_kc_2/controller/EstudianteControllerRestFul.java
@@ -5,10 +5,7 @@ import com.uce.edu.ec.programacion_pw_u3_kc_2.service.IEstudianteService;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/estudiantes")
@@ -17,19 +14,31 @@ public class EstudianteControllerRestFul {
     @Autowired
     private IEstudianteService iEstudianteService;
 
+    @PostMapping
     public void insertarEstudiante(Estudiante estudiante) {
     }
 
     //estudiantes/buscar
-    @GetMapping(path = "/buscar/{id}")
+    @GetMapping(path = "/{id}")
     public Estudiante buscarEstudiante(@PathVariable("id") Integer id) {
         return this.iEstudianteService.encontrarEstudiante(id);
     }
 
-    public void actualizarEstudiante(Estudiante estudiante) {
+    @PutMapping(path = "/{id}")
+    public void actualizarEstudiante(@PathVariable("id") Estudiante estudiante) {
     }
 
-    public void borrarEstudiante(Integer id) {
+    @PutMapping
+    public void actualizarTodos(Estudiante estudiante) {
+    }
+
+    @DeleteMapping(path = "{id}")
+    public void borrarEstudiante(@PathVariable("id") Integer id) {
+        this.iEstudianteService.borrarEstudiante(id);
+    }
+
+    @DeleteMapping
+    public void borrarTodos(Integer id) {
     }
 
 }
